71d99285cc89158e4d66c423e586c0ba48e7dbce
Andreas Noack <andreas@noack.dk>
2019-03-25 12:43:35 +0100

    Add some functionality for betweenness

--- src/ConScape.jl
+++ src/ConScape.jl
@@ -1,30 +1,32 @@
 module ConScape
 
-    using SparseArrays, PyPlot
+    using SparseArrays, PyPlot, LightGraphs, SimpleWeightedGraphs
+    using LinearAlgebra
 
     mutable struct Grid
         shape::Tuple{Int,Int}
         A::SparseMatrixCSC{Float64,Int}
         id_to_grid_coordinate_list::Vector{Tuple{Int,Int}}
+        qualities::Vector{Float64}
     end
 
-    function Grid(;shape=nothing, qualities=nothing, nhood_size=8)
+    function Grid(;shape=nothing, qualities::Vector=ones(prod(shape)), nhood_size=8)
         N_grid = prod(shape)
         n_cols = shape[2]
         Grid(shape,
              _generateA(shape..., nhood_size),
-             _set_id_to_grid_coordinate_list(N_grid, n_cols))
+             _set_id_to_grid_coordinate_list(N_grid, n_cols),
+             qualities)
     end
 
     """
     simulate a permeable wall
     """
-    function perm_wall_sim(;grid_shape=(30,60), Q=1, A=0.5, ww=3, wp=0.5, cw=(3,3), cp=(0.35,0.7))
+    function perm_wall_sim(;grid_shape=(30,60), Q=1, A=0.5, ww=3, wp=0.5, cw=(3,3), cp=(0.35,0.7), qualities=fill(Q, prod(grid_shape)))
 
         # 1. initialize landscape
         n_rows, n_cols = grid_shape
         N = n_rows*n_cols
-        qualities = ones(N)*Q
         g = Grid(shape=grid_shape, qualities=qualities)
         g.A = A * g.A
 
@@ -165,6 +167,12 @@ module ConScape
         g.A = A
     end
 
+    function mapnz(f, A::SparseMatrixCSC)
+        B = copy(A)
+        map!(f, B.nzval, A.nzval)
+        return B
+    end
+
     function plot_outdegrees(g::Grid; cmap="cool")
         values = sum(g.A, dims=2)
         canvas = zeros(g.shape...)
@@ -172,5 +180,243 @@ module ConScape
             canvas[g.id_to_grid_coordinate_list[i]...] = v
         end
         imshow(canvas, cmap=cmap)
+    end
+
+
+    struct HabitatAnalysis
+        g::Grid
+        C::SparseMatrixCSC{Float64,Int}
+        P_ref::SparseMatrixCSC{Float64,Int}
+        landmarks::Vector{Int}
+    end
+
+    HabitatAnalysis(g::Grid,
+                    costfunction::Function,
+                    landmarks::AbstractVector = 1:size(g.A, 1)) =
+                        HabitatAnalysis(g,
+                                        mapnz(costfunction, g.A),
+                                        _compute_Pref(g.A),
+                                        landmarks)
+
+    ```
+    Compute the I - W matrix used in the free energy distance.
+
+    Parameters:
+    - β: If given separately, do not change self.I_W but only return I_W according to the given beta.
+            If None, then beta=self.beta and this computes and stores self.I_W
+
+    Returns:
+    - I_W: I-W, where I is identity matrix and W is matrix with elements w_ij = P_ij*exp(-beta*c_ij)
+    ```
+    function _compute_W(h::HabitatAnalysis; β=nothing)
+        if β === nothing
+            throw(ArgumentError("β must be set to a value"))
+        end
+
+        A = h.g.A
+        C = h.C
+        P_ref = h.P_ref
+        N = LinearAlgebra.checksquare(A)
+
+        # Compute W:
+        expbC = mapnz(t -> exp(-β*t), C)
+        W = P_ref .* expbC
+
+        return W
+    end
+
+    function _compute_Pref(A::SparseMatrixCSC)
+        inv_degrees = vec(sum(A, dims=1))
+        map!(t -> ifelse(t > 0, inv(t), t), inv_degrees, inv_degrees)
+        D_inv = spdiagm(0 => inv_degrees)
+        return D_inv*A
+    end
+
+    ```
+    Compute full RSP betweenness of all nodes weighted by quality
+    TODO: Verify that this works
+    ```
+    function RSP_full_betweenness_qweighted(h::HabitatAnalysis; β=nothing)
+        if β === nothing
+            throw(ArgumentError("β must be set to a value"))
+        else
+            I_W = I - _compute_W(h, β=β)
+        end
+
+        Z = inv(Matrix(I_W))
+        # tol = 1/1000000 #used to avoid 0 division
+        # Zdiv = map(t -> ifelse(t > tol, inv(t), inv(tol)), Z)
+        Zdiv = inv.(Z)
+
+        # Qs = Diagonal(h.g.qualities)
+        # Qt = Qs
+        # sm = sum(h.g.qualities)
+
+        # D_Zdiv = Diagonal(diag(Zdiv * Qt))
+
+        # bet = diag( Z * (Qt * Zdiv' * Qs - sm * D_Zdiv) * Z )
+
+        q = h.g.qualities
+        sm = sum(q)
+        D_Zdiv = Diagonal(diag(Zdiv) .* q)
+        bet = diag(Z * (q .* Zdiv' .* q' .- sm .* D_Zdiv) * Z)
+
+        return bet
+    end
+
+    ```
+    Compute full RSP betweenness of all nodes weighted with proximity
+    TODO: Verify that this works
+    ```
+    function RSP_full_betweenness_kweighted(h::HabitatAnalysis, β=nothing)
+        if β === nothing
+            throw(ArgumentError("β must be set to a value"))
+        else
+            I_W = Matrix(I - _compute_W(h, β=β))
+        end
+
+        Z = inv(I_W)
+
+        # if Z.min() == 0:
+        #     Z[Z < 1./1000000] = 1./1000000 #used to avoid 0 division
+        Zdiv = inv.(Z)
+
+        q = h.g.qualities
+
+        K = _similarities_all2L(h)
+
+        K = Qs*(K*Qt)
+        e = ones(length(q), length(q))
+
+        # TODO: Check that this is written correctly, especially concerning the elementwise and dot products:
+        bet = diag( Z * ((Zdiv*K)' .- diag(Zdiv) * diag(K' * e)) * Z )
+
+        return bet
+    end
+
+    ```
+    Compute RSP expected costs or RSP dissimilarities from all nodes to landmarks.
+
+    TODO: Not yet implemented as from_landmarks_to_all. This requires computation of the diagonal of Z, which is not trivial.
+    TODO: Do something smarter with return_mean_D_KL.
+    ```
+    function RSP_dissimilarities_to(h::HabitatAnalysis; β=nothing, destinations=h.landmarks, return_mean_D_KL=true, algorithm=:batch)
+        tic = time()
+
+        N = size(h.g.A, 1)
+
+        N_destinations = length(destinations)
+
+        W = _compute_W(h, β=β)
+
+        CW = h.C .* W
+
+        if length(destinations) == N
+            # Compute all distances.
+            #
+            # TODO: Remove consideration of 0-quality nodes as they may cause unnecessary inf-problems
+            #
+            @info("Computing Z...")
+            Z   = inv(Matrix(I - W))
+
+            # TODO (maybe):
+            # if self.return_mean_D_KL
+
+            if any(iszero, Z)
+                Z_has_zeros = true
+                inf_idx = sparse(Z.==0)
+                @warn("Z contains zeros! This will cause some distances to become infinite.")
+            else
+                Z_has_zeros = false
+            end
+
+            @info("Computing (C*W)Z...")
+            D = Matrix(CW)
+            # D = D*Z
+            D = Z'\Matrix(CW)'
+
+            @info("Computing Z(C*W)Z...")
+            D = Z*D
+
+            @info("Computing Z(C*W)Z/Z...")
+            D ./= Z
+
+            @info("Computing D - e*diag(D).T")
+            if Z_has_zeros
+                D[Matrix(inf_idx)] .= Inf
+            end
+
+            # D above actually gives the expected costs of non-hitting paths
+            # from i to j.
+
+            # Expected costs of hitting paths avoiding possible 'Inf-Inf':
+            diag_vec = diag(D)
+            inf_idx = isinf.(diag_vec)
+
+            diag_vec = reshape(diag_vec, 1, length(diag_vec)) # Make into a 1xN-vector
+            D .-= diag_vec
+
+            D[:,inf_idx] .= Inf
+
+            if return_mean_D_KL
+                @warn("Computing mean KL-divergence")
+                Z_diag_inv = inv(Diagonal(Z))
+                D_KL = Z*Z_diag_inv
+                D_KL .= .-log.(D_KL) .- β.*D
+                D_KL[:,inf_idx] .= 0
+
+                mean_D_KL = (h.g.qualities'*D_KL)*h.g.qualities
+            end
+
+            toc = time() - tic
+            @info("RSP expected costs computed in $toc seconds")
+
+
+        elseif algorithm == :batch
+            I_dest = zeros(N, N_destinations)
+            I_dest[destinations, 1:N_destinations] .= 1
+
+            Z_dest = I_W\I_dest
+
+            unconnected_pairs = Z_dest .== 0
+            if any(unconnected_pairs)
+                @warn("Some values of Z_dest are zero either because of unconnected nodes or a high value of beta, which may lead to infinite distances.")
+            end
+            Q_dest = CW*Z_dest
+
+            D = I_W\Q_dest
+
+            D ./= Z_dest
+            D[unconnected_pairs] .= 0
+            D_dest = D[destinations, 1:N_destinations]
+            D -= D_dest
+            D[unconnected_pairs] .= Inf
+
+        elseif algorithm == :sequential
+            D = zeros(N, N_destinations)
+            for (i,t) in enumerate(destinations)
+                # I_W_t = I_W
+                # I_W_t[t,:] = 0  Not really needed
+
+                e_t = zeros(N)
+                e_t[t] = 1
+                z_t = I_W\e_t
+                unconnected_nodes = z_t .== 0
+                if any(unconnected_nodes)
+                    @warn("Some values of z_t are zero either because of unconnected nodes or a high value of beta, which may lead to infinite distances.")
+                end
+                z_t_2d = reshape(z_t, 1, length(z_t))
+                q_t = CW*z_t_2d
+                c_t = I_W\q_t
+                c_t ./= z_t
+                c_t[unconnected_nodes] .= 0
+                c_t -= c_t[t]
+
+                D[:,i] = c_t
+                D[unconnected_nodes,i] .= Inf
+            end
+        end
+
+        return D
     end
 end
