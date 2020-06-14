258da3e4031b2a42854c91f618233d3708eef0d7
Andreas Noack <andreas@noack.dk>
2020-06-14 21:00:29 +0200

    Add more regression tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,11 @@ datadir = joinpath(@__DIR__(), "..", "data")
     β = 0.1
 
     affinities, _ = ConScape.readasc(joinpath(datadir, "affinities_$landscape.asc"))
+    @test affinities[30:32, 30:32] == [
+        0.680433618733817 0.548574246296093 0.0513858082558727
+        0.725725747086108 0.425760212528985 0.0378853709637769
+        0.658316359706223 0.549403912723064 0.247374645175878]
+
     qualities , _ = ConScape.readasc(joinpath(datadir, "qualities_$landscape.asc"))
 
     g = ConScape.Grid(size(affinities)...,
@@ -14,16 +19,52 @@ datadir = joinpath(@__DIR__(), "..", "data")
         qualities=qualities
         )
 
+    @testset "Grid fields" begin
+        @test g.ncols == 59
+        @test g.nrows == 44
+        @test g.A[1000:1002, 1000:1002] == [
+            0.0               0.557963581550866 0.0
+            0.607917269319296 0.0               0.273319838512689
+            0.0               0.557963581550866 0.0]
+        @test g.id_to_grid_coordinate_list[1000:1002] == [
+            CartesianIndex(37, 41), CartesianIndex(38, 41), CartesianIndex(39, 41)]
+        @test g.source_qualities[30:32, 30:32] == [
+            0.151232712594792 0.146546460358077  0.00748122241586316
+            0.170905506269773 0.0532220626743219 0.00309705330379074
+            0.10284506027383  0.059244283127482  0.0361260830667015]
+        @test g.target_qualities.nzval[1:3] == [
+            0.00128399959066883, 0.00289564372117942, 0.000117460697806407]
+    end
+
     @testset "Grid plotting" begin
         @test ConScape.plot_indegrees(g) isa ConScape.Plots.Plot
         @test ConScape.plot_outdegrees(g) isa ConScape.Plots.Plot
         @test ConScape.plot_values(g,ones(length(g.id_to_grid_coordinate_list))) isa ConScape.Plots.Plot
     end
 
-    h = ConScape.GridRSP(g, β=β)
+    grsp = ConScape.GridRSP(g, β=β)
+
+    @testset "GridRSP fields" begin
+        @test grsp.C.nzval[end-2:end] ≈ [
+            17.339919976251554
+            16.99334638597158
+            17.339919976251554]
+        @test grsp.Pref.nzval[end-2:end] ≈ [
+            0.00031266870414554466,
+            0.00018055948867712768,
+            0.0001228960184654185]
+        @test grsp.W.nzval[end-2:end] ≈ [
+            5.521044625610329e-5,
+            3.300719810371289e-5,
+            2.1700745653826738e-5]
+        @test grsp.Z[100:102,100:102] ≈ [
+            0.00016367398568399748 0.00015329797258788392 0.00023129137088760695
+            6.432239855543766e-5   5.4944307498337036e-5  7.336349355801132e-5
+            2.421703672476501e-5   1.953276891351377e-5   2.4440692415300965e-5]
+    end
 
     @testset "Test mean_kl_divergence" begin
-        @test ConScape.mean_kl_divergence(h) ≈ 323895.3828183995
+        @test ConScape.mean_kl_divergence(grsp) ≈ 323895.3828183995
     end
 
     @testset "test adjacency creation with $nn neighbors, $w weighting and $mt" for
@@ -40,43 +81,52 @@ datadir = joinpath(@__DIR__(), "..", "data")
     end
 
     @testset "Test betweenness" begin
-        bet = ConScape.betweenness_kweighted(h, diagvalue=1.)
-        @test bet[21:23, 31:33] ≈ [
-            0.04063917813171917 0.06843246983487516 0.08862506281612659
-            0.03684621201600996 0.10352876485995872 0.1255652231824746
-            0.03190640567704462 0.13832814750469344 0.1961393152256104]
-
-        # Check that summed edge betweennesses corresponds to node betweennesses:
-        bet_edge = ConScape.edge_betweenness_kweighted(h, diagvalue=1.)
-        bet_edge_sum = fill(NaN, h.g.nrows, h.g.ncols)
-        for (i, v) in enumerate(sum(bet_edge,dims=2))
-            bet_edge_sum[h.g.id_to_grid_coordinate_list[i]] = v
+        @testset "q-weighted" begin
+            bet = ConScape.betweenness_qweighted(grsp)
+            @test bet[21:23, 21:23] ≈ [
+                1930.1334372152335  256.91061166392745 2866.2998374065373
+                4911.996715311025  1835.991238248377    720.755518530375
+                4641.815380725279  3365.3296878569213   477.1085971945757]
         end
-        @test bet_edge_sum[21:23, 31:33] ≈ bet[21:23, 31:33]
 
-        # This is a regression test based on values that we currently believe to be correct
-        bet = ConScape.betweenness_kweighted(h, invcost=t -> exp(-t/50))
-        @test bet[21:23, 31:33] ≈ [
-            980.5828087688377 1307.981162399926 1602.8445739784497
-            826.0710054834001 1883.0940077789735 1935.4450344630702
-            676.9212075214159 2228.2700913772774 2884.0409495023364]
+        @testset "k-weighted" begin
+            bet = ConScape.betweenness_kweighted(grsp, diagvalue=1.)
+            @test bet[21:23, 31:33] ≈ [
+                0.04063917813171917 0.06843246983487516 0.08862506281612659
+                0.03684621201600996 0.10352876485995872 0.1255652231824746
+                0.03190640567704462 0.13832814750469344 0.1961393152256104]
 
-        @test ConScape.betweenness_kweighted(h, invcost=one)[g.id_to_grid_coordinate_list] ≈
-            ConScape.betweenness_qweighted(h)[g.id_to_grid_coordinate_list]
+            # Check that summed edge betweennesses corresponds to node betweennesses:
+            bet_edge = ConScape.edge_betweenness_kweighted(grsp, diagvalue=1.)
+            bet_edge_sum = fill(NaN, grsp.g.nrows, grsp.g.ncols)
+            for (i, v) in enumerate(sum(bet_edge,dims=2))
+                bet_edge_sum[grsp.g.id_to_grid_coordinate_list[i]] = v
+            end
+            @test bet_edge_sum[21:23, 31:33] ≈ bet[21:23, 31:33]
 
-        @test ConScape.edge_betweenness_kweighted(h, invcost=one) ≈
-            ConScape.edge_betweenness_qweighted(h)
+            # This is a regression test based on values that we currently believe to be correct
+            bet = ConScape.betweenness_kweighted(grsp, invcost=t -> exp(-t/50))
+            @test bet[21:23, 31:33] ≈ [
+                980.5828087688377 1307.981162399926 1602.8445739784497
+                826.0710054834001 1883.0940077789735 1935.4450344630702
+                676.9212075214159 2228.2700913772774 2884.0409495023364]
 
+            @test ConScape.betweenness_kweighted(grsp, invcost=one)[g.id_to_grid_coordinate_list] ≈
+                ConScape.betweenness_qweighted(grsp)[g.id_to_grid_coordinate_list]
+
+            @test ConScape.edge_betweenness_kweighted(grsp, invcost=one) ≈
+                ConScape.edge_betweenness_qweighted(grsp)
+        end
     end
 
     @testset "functionality" begin
-        hf = ConScape.functionality(h)
+        hf = ConScape.functionality(grsp)
         @test hf isa SparseMatrixCSC
-        @test size(hf) == size(h.g.source_qualities)
+        @test size(hf) == size(grsp.g.source_qualities)
     end
 
     @testset "mean_lc_kl_divergence" begin
-        @test ConScape.ConScape.mean_lc_kl_divergence(h) ≈ 1.5660600315073947e6
+        @test ConScape.ConScape.mean_lc_kl_divergence(grsp) ≈ 1.5660600315073947e6
     end
 
     @testset "Show methods" begin
@@ -85,7 +135,7 @@ datadir = joinpath(@__DIR__(), "..", "data")
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/plain", h)
+        show(b, "text/plain", grsp)
         @test occursin("GridRSP", String(take!(b)))
 
         b = IOBuffer()
@@ -93,7 +143,7 @@ datadir = joinpath(@__DIR__(), "..", "data")
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/html", h)
+        show(b, "text/html", grsp)
         @test occursin("GridRSP", String(take!(b)))
     end
 end
@@ -116,34 +166,33 @@ end
 
 
 
-
-    h = ConScape.GridRSP(g, cost=ConScape.MinusLog(), β=β)
+    grsp = ConScape.GridRSP(g, cost=ConScape.MinusLog(), β=β)
 
     @testset "Test mean_kl_divergence" begin
-        @test ConScape.mean_kl_divergence(h) ≈ 2.4405084252728125e13
+        @test ConScape.mean_kl_divergence(grsp) ≈ 2.4405084252728125e13
     end
 
     @testset "Test betweenness" begin
         # Check that summed edge betweennesses corresponds to node betweennesses:
-        bet_node = ConScape.betweenness_qweighted(h)
-        bet_edge = ConScape.edge_betweenness_qweighted(h)
-        bet = fill(NaN, h.g.nrows, h.g.ncols)
+        bet_node = ConScape.betweenness_qweighted(grsp)
+        bet_edge = ConScape.edge_betweenness_qweighted(grsp)
+        bet = fill(NaN, grsp.g.nrows, grsp.g.ncols)
         for (i, v) in enumerate(sum(bet_edge,dims=2))
-            bet[h.g.id_to_grid_coordinate_list[i]] = v
+            bet[grsp.g.id_to_grid_coordinate_list[i]] = v
         end
 
         @test bet ≈ bet_node
     end
 
     @testset "functionality" begin
-        @test ConScape.ConScape.functionality(h, diagvalue=0.0)[28:30,58:60]' ≈
+        @test ConScape.ConScape.functionality(grsp, diagvalue=0.0)[28:30,58:60]' ≈
             [11082.654882969266 2664.916100189486 89.420910249988
              10340.977912804196 2465.918728844169 56.970111157896
              11119.132467660969 2662.969749775032 33.280379014217]
     end
 
     @testset "mean_lc_kl_divergence" begin
-        @test ConScape.ConScape.mean_lc_kl_divergence(h) ≈ 1.0667623231698838e14
+        @test ConScape.ConScape.mean_lc_kl_divergence(grsp) ≈ 1.0667623231698838e14
     end
 
     @testset "Coarse graining: merging pixels to landmarks" begin
@@ -167,7 +216,7 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/plain", h)
+        show(b, "text/plain", grsp)
         @test occursin("GridRSP", String(take!(b)))
 
         b = IOBuffer()
@@ -175,7 +224,7 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/html", h)
+        show(b, "text/html", grsp)
         @test occursin("GridRSP", String(take!(b)))
     end
 end
@@ -200,7 +249,7 @@ end
         @test ConScape.plot_values(g,ones(length(g.id_to_grid_coordinate_list))) isa ConScape.Plots.Plot
     end
 
-    h = ConScape.GridRSP(g, β=0.2)
+    grsp = ConScape.GridRSP(g, β=0.2)
 
     @testset "Show methods" begin
         b = IOBuffer()
@@ -208,7 +257,7 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/plain", h)
+        show(b, "text/plain", grsp)
         @test occursin("GridRSP", String(take!(b)))
 
         b = IOBuffer()
@@ -216,12 +265,12 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/html", h)
+        show(b, "text/html", grsp)
         @test occursin("GridRSP", String(take!(b)))
     end
 
     @testset "Landmark approach" begin
-        @test ConScape.betweenness_qweighted(h)[9:11, 30:32] ≈
+        @test ConScape.betweenness_qweighted(grsp)[9:11, 30:32] ≈
             [1.35257193796979e9 1.3112254944853191e9 1.3525448385844798e9
              1.7383632661402326e9 1.9571251417867596e9 1.7385247019409044e9
              1.352382919812123e9 1.3103077614483771e9 1.3520848636655023e9]
@@ -250,7 +299,7 @@ end
         @test ConScape.plot_values(g,ones(length(g.id_to_grid_coordinate_list))) isa ConScape.Plots.Plot
     end
 
-    h = ConScape.GridRSP(g, β=0.2)
+    grsp = ConScape.GridRSP(g, β=0.2)
 
     @testset "Show methods" begin
         b = IOBuffer()
@@ -258,7 +307,7 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/plain", h)
+        show(b, "text/plain", grsp)
         @test occursin("GridRSP", String(take!(b)))
 
         b = IOBuffer()
@@ -266,12 +315,12 @@ end
         @test occursin("Grid", String(take!(b)))
 
         b = IOBuffer()
-        show(b, "text/html", h)
+        show(b, "text/html", grsp)
         @test occursin("GridRSP", String(take!(b)))
     end
 
     @testset "Landmark approach" begin
-        @test ConScape.betweenness_kweighted(h)[9:11, 30:32] ≈
+        @test ConScape.betweenness_kweighted(grsp)[9:11, 30:32] ≈
             [1.6153674943888483e6 693690.2564610258    1.6097137526944755e6
              1.8168095466336345e6 1.8166090537379407e6 1.8108940319968446e6
              1.41753770380708e6   668884.5700736387    1.412290291817482e6 ]
@@ -326,21 +375,21 @@ end
       l,
       neighbors=ConScape.N4,
       matrix_type=ConScape.CostMatrix)
-    h = ConScape.GridRSP(g, C=C, β=2.)
+    grsp = ConScape.GridRSP(g, C=C, β=2.)
 
-    @test maximum(abs.(ConScape.free_energy_distance(h) - [
+    @test maximum(abs.(ConScape.free_energy_distance(grsp) - [
       0.0       1.34197   1.34197   2.34197
       1.34197   0.0       2.34197   1.34197
       1.34197   2.34197   0.0       1.34197
       2.34197   1.34197   1.34197   0.0     ])) < 1e-5
 
-    @test maximum(abs.(ConScape.survival_probability(h) - [
+    @test maximum(abs.(ConScape.survival_probability(grsp) - [
       1.0         0.0682931   0.0682931   0.00924246
       0.0682931   1.0         0.00924246  0.0682931
       0.0682931   0.00924246  1.0         0.0682931
       0.00924246  0.0682931   0.0682931   1.0    ])) < 1e-5
 
-    @test maximum(abs.(ConScape.power_mean_proximity(h) - [
+    @test maximum(abs.(ConScape.power_mean_proximity(grsp) - [
       1.0        0.261329   0.261329   0.0961377
       0.261329   1.0        0.0961377  0.261329
       0.261329   0.0961377  1.0        0.261329
@@ -353,9 +402,9 @@ end
     q = rand(4, 4)
 
     g = ConScape.Grid(size(l)..., landscape=ConScape.graph_matrix_from_raster(l))
-    h = ConScape.GridRSP(g, β=0.2)
+    grsp = ConScape.GridRSP(g, β=0.2)
 
-    @test ConScape.betweenness_kweighted(h) == ConScape.betweenness_kweighted(h; invcost=t -> exp(-t))
+    @test ConScape.betweenness_kweighted(grsp) == ConScape.betweenness_kweighted(grsp; invcost=t -> exp(-t))
 end
 
 @testset "least cost kl divergence" begin
@@ -382,8 +431,8 @@ end
          4.330475309063122  2.027890216069076   0.7949298748698876  0.6007738604289302  0.0               ]
 
     g = ConScape.perm_wall_sim(30, 60, corridorwidths=(3,2))
-    h = ConScape.GridRSP(g, cost=ConScape.MinusLog(), β=0.2)
-    @test ConScape.least_cost_kl_divergence(h, (25,50))[10,10] ≈ 80.63375074079197
+    grsp = ConScape.GridRSP(g, cost=ConScape.MinusLog(), β=0.2)
+    @test ConScape.least_cost_kl_divergence(grsp, (25,50))[10,10] ≈ 80.63375074079197
 end
 
 # FIXME! Computation is currently very slow so we have to use a reduced landscape
@@ -392,8 +441,8 @@ end
     g = ConScape.perm_wall_sim(m, n, corridorwidths=(2,2),
       # Qualities decrease by row
       qualities=copy(reshape(collect(m*n:-1:1), n, m)'))
-    h = ConScape.GridRSP(g, β=0.2)
-    @test sum(ConScape.criticality(h).nzval .< -1e-5) == 0
+    grsp = ConScape.GridRSP(g, β=0.2)
+    @test sum(ConScape.criticality(grsp).nzval .< -1e-5) == 0
 end
 
 
@@ -412,16 +461,16 @@ end
 
     g.A = 0.35 * g.A
 
-    h = ConScape.GridRSP(g, β=0.2, cost=cost)
+    grsp = ConScape.GridRSP(g, β=0.2, cost=cost)
 
-    S_comp = ConScape.LF_sensitivity(h, diagvalue=1.)[1]
-    S_simu = ConScape.LF_sensitivity_simulation(h, diagvalue=1.)[1]
+    S_comp = ConScape.LF_sensitivity(grsp, diagvalue=1.)[1]
+    S_simu = ConScape.LF_sensitivity_simulation(grsp, diagvalue=1.)[1]
 
     @test sum(abs.(S_comp - S_simu)./maximum(abs.(S_comp))) ≈ 0 atol=5e-3
 
 
-    S_comp = ConScape.LF_power_mean_sensitivity(h)[1]
-    S_simu = ConScape.LF_power_mean_sensitivity_simulation(h)[1]
+    S_comp = ConScape.LF_power_mean_sensitivity(grsp)[1]
+    S_simu = ConScape.LF_power_mean_sensitivity_simulation(grsp)[1]
 
     @test sum(abs.(S_comp - S_simu)./maximum(abs.(S_comp))) ≈ 0 atol=5e-3
 
