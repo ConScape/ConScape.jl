a267ceecd5eacfc53d18ee340e86d3a0a633497e
Rafael Schouten <rafaelschouten@gmail.com>
2025-06-02 12:42:10 +0200

    no threading in LinearSolver

--- ext/ConScapeLinearSolveExt.jl
+++ ext/ConScapeLinearSolveExt.jl
@@ -6,53 +6,22 @@ import CommonSolve
 import LinearAlgebra
 
 # Now LinearSolve.jl is imported, add a working LinearSolver constructor
-ConScape.LinearSolver(args::A, kw::K, threaded::Bool) where {A<:Tuple,K} =
-    LinearSolver{A,K}(args, kw, threaded)
+ConScape.LinearSolver(args::A, kw::K) where {A<:Tuple,K} =
+    LinearSolver{A,K}(args, kw)
 
 function CommonSolve.init(solver::LinearSolver, A::AbstractMatrix)
     b = ones(eltype(A), size(A, 2))
     # Define and initialise the linear problem
     linprob = LinearProblem(A, b)
     linsolve = init(linprob, solver.args...; solver.keywords...)
-    # TODO what is needed here?
-    # Create a channel to store problem b vectors for threads
-    # see https://juliafolds2.github.io/OhMyThreads.jl/stable/literate/tls/tls/
-    # if ConScape.isthreaded(solver)
-    #     nbuffers = Threads.nthreads()
-    #     channel = Channel{Tuple{typeof(linsolve),Vector{Float64}}}(nbuffers)
-    #     for i in 1:nbuffers
-    #         # TODO fix this in LinearSolve.jl with batching
-    #         # We should not need to `deepcopy` the whole problem we 
-    #         # just need to replicate the specific workspace arrays 
-    #         # that will cause race conditions.
-    #         # But currently there is no parallel mode for LinearSolve.jl
-    #         # See https://github.com/SciML/LinearSolve.jl/issues/552
-    #         put!(channel, (deepcopy(linsolve), Vector{eltype(A)}(undef, size(A, 2))))
-    #     end
-    #     return channel
-    # else
     return linsolve
-    # end
 end
 
 function LinearAlgebra.ldiv!(s::LinearSolver, B, init, B_copy)
-    # TODO: for now we define a Z matrix, but later modify ops 
-    # to run column by column without materialising Z
-    if ConScape.isthreaded(s)
-        channel = init
-        # Get column memory from the channel
-        linsolve = take!(channel)
-        # Update solver with new b values
-        reinit!(linsolve; b=vec(B_copy), reuse_precs=true)
-        sol = LinearSolve.solve!(vec(B), linsolve, s.args...; s.keywords...)
-        vec(B) .= sol.u
-        put!(channel, linsolve)
-    else
-        linsolve = init
-        reinit!(linsolve; b=vec(B_copy), reuse_precs=true)
-        sol = LinearSolve.solve(linsolve, s.args...; s.keywords...)
-        vec(B) .= sol.u
-    end
+    linsolve = init
+    reinit!(linsolve; b=vec(B_copy), reuse_precs=true)
+    sol = LinearSolve.solve(linsolve, s.args...; s.keywords...)
+    vec(B) .= sol.u
     return B
 end
 
