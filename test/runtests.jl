c189bccdbc847a7bfbc9bbac09d4326a17b87279
Rafael Schouten <rafaelschouten@gmail.com>
2025-11-11 14:20:20 +1100

    completely remove JET its too broken in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,12 @@
 using ConScape
 using Test
 using Aqua
-# using JET
 using SafeTestsets
 
 @testset "ConScape.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(ConScape)
     end
-    # TODO: The ConScapeProblem constructor has minor problems
-    # @testset "Code linting (JET.jl)" begin
-        # JET.test_package(ConScape; target_defined_modules = true)
-    # end
     @safetestset "Workspaces" begin include("workspaces.jl") end
     @safetestset "Graphs" begin include("graph_generation.jl") end
     # @safetestset "basics" begin include("basics.jl") end
